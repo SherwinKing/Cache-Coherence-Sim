510e210fc947d1442f480d04c4cff5d5350553f9
SherwinKing <jinsheng1997@outlook.com>
2021-05-07 09:50:14 +0800

    Add CacheController abstract class

---
+++ src/CacheController.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_CSIM_CACHECONTROLLER_H
+#define PROJECT_CSIM_CACHECONTROLLER_H
+
+
+#include "Response.h"
+#include "Request.h"
+
+class CacheController {
+public:
+    virtual void runCacheOp(long tag, long setInd, char operation);
+    virtual Response deliverRequest(Request request);
+};
+
+
+#endif //PROJECT_CSIM_CACHECONTROLLER_H
