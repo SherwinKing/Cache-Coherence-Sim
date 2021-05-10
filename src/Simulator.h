fadfe5899de198c20b03edda221edbc8e57e95d8
SherwinKing <jinsheng1997@outlook.com>
2021-05-10 09:57:58 +0800

    Update Simulator

--- src/Simulator.h
+++ src/Simulator.h
@@ -9,13 +9,21 @@
 
 class Simulator {
 private:
-    std::vector<Cache> caches;
-    Statistics statistics;
+    std::vector<CacheController> cacheControllers;
+    Statistics & statistics;
     Interconnection interconnection;
+    int getOptions(int argc, char* argv[]);
 
-    void runTraceLine(std::string);
+    int s;  // Number of set index bits (2^s is the cache size, or the number of sets in cache)
+    int E;  // Associativity
+    int b;  // Number of block bits
+
+    int verbose;
+
+    std::ifstream traceFileStream;
 
 public:
+
     void runTraceFile(std::ifstream);
 };
 
