d6c696425a6c50650ce08efcd13090aa6f212c21
qiuje <qiuje98@outlook.com>
2021-05-10 17:37:42 -0500

    Add evictionHandler function for directory. Add sourceID argument for requestHandler. Add a destructor. Change all functions to pure virtual

--- src/NodeController.h
+++ src/NodeController.h
@@ -5,13 +5,18 @@
 #include "Response.h"
 #include "Request.h"
 #include "CacheSet.h"
+#include <string>
 
 class NodeController {
 private:
-    virtual void transitCacheLineState(CacheLine &cacheLine, long cacheAddress, Request request);
+    virtual void transitCacheLineState(CacheLine &cacheLine, long cacheAddress, Request request) = 0;
 public:
-    virtual void runCacheOp(long address, std::string operation, int timeStamp);
-    virtual Response requestHandler(Request request);
+    virtual void runCacheOp(long address, std::string operation, int timeStamp) = 0;
+    virtual Response requestHandler(Request request, int sourceID) = 0;
+    // directory function
+    virtual void evictionHandler(int sourceID, long setID, long tag, int token) = 0;
+
+    virtual ~NodeController() {}
 };
 
 
