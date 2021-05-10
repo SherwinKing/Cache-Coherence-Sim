521167723d081cfc7d3cc4c4347feb9de830c830
SherwinKing <jinsheng1997@outlook.com>
2021-05-10 09:58:12 +0800

    Update SnoopingCacheContronller

--- src/SnoopingCacheController.h
+++ src/SnoopingCacheController.h
@@ -12,15 +12,29 @@
 
 class SnoopingCacheController: public CacheController {
 private:
+    int id;
+    int s;  // Number of set index bits (2^s is the cache size, or the number of sets in cache)
+    int E;  // Associativity
+    int b;  // Number of block bits
+
+    long setIndMask;
+    long tagMask;
+
+    const CoherenceType coherenceType = SNOOPING;
+    int processorID;
     Cache cache;
     Interconnection interconnection;
-    Statistics statistics;
+    Statistics & statistics;
+
+    void transitCacheLineState(CacheLine &cacheLine, long cacheAddress, Request request) override;
+
+    void updateCacheLine(CacheLine &cacheLineToUpdate, long newTag, std::string operation, int timeStamp);
 
 public:
-    void runCacheOp(long tag, long setInd, char operation) override;
+    void runCacheOp(long address, std::string operation, int timeStamp) override;
 
     Response requestHandler(Request request) override;
-    
+
 };
 
 
