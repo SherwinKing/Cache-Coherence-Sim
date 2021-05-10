6380dd49f71086eccc93042beba84fbf29387f23
SherwinKing <jinsheng1997@outlook.com>
2021-05-10 10:13:33 +0800

    Update sendRequest, support directory

--- src/Interconnection.cpp
+++ src/Interconnection.cpp
@@ -1,8 +1,17 @@
 #include "Interconnection.h"
 
 int Interconnection::sendRequest(int sourceID, int receiverID, Request request, Response & response) {
+    int latency;
+    // If receiver is special (used for directory node)
+    if (receiverID == -1) {
+        receiverID = cacheVector.size() - 1;
+        latency = 1;
+    } else {
+        latency = getLatency(sourceID, receiverID);
+    }
+
     response = cacheVector[receiverID].requestHandler(request);
-    return getLatency(sourceID, receiverID);
+    return latency;
 }
 
 int Interconnection::getLatency(int nodeID1, int nodeID2) {
