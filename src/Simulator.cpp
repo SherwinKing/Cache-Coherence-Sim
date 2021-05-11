832ce3163abf39f69204acb7629ed7cdfba3e02c
SherwinKing <jinsheng1997@outlook.com>
2021-05-11 14:11:44 +0800

    Update Simulator

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -1,59 +1,80 @@
-#include <sstream>
-#include <fstream>
-#include <getopt.h>
 #include "Simulator.h"
 
 
-int Simulator::getOptions(int argc, char **argv) {
-    int option;
-    while( (option = getopt(argc, argv, "hvs:E:b:t:")) != -1 ){
-        switch(option){
-            case 'v':
-                verbose = 1;
-                break;
-            case 's':
-                s = atoi(optarg);
-                break;
-            case 'E':
-                E = atoi(optarg);
-                break;
-            case 'b':
-                b = atoi(optarg);
-                break;
-            case 't':
-                traceFileStream.open("example.txt");
-                if (!traceFileStream.is_open())
-                {
-                    std::cerr << "Error opening trace file";
-                    traceFileStream.close();
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            default:
-                fprintf(stderr,
-                        "Usage: ./csim [-v] -s <s> -E <E> -b <b> -t <tracefile>");
-                exit(EXIT_FAILURE);
-        }
-    }
-    return 0;
-}
+//int Simulator::getOptions(int argc, char **argv) {
+//    int option;
+//    while( (option = getopt(argc, argv, "hvs:E:b:t:")) != -1 ){
+//        switch(option){
+//            case 'v':
+//                verbose = 1;
+//                break;
+//            case 's':
+//                s = atoi(optarg);
+//                break;
+//            case 'E':
+//                E = atoi(optarg);
+//                break;
+//            case 'b':
+//                b = atoi(optarg);
+//                break;
+//            case 't':
+//                traceFileStream.open("example.txt");
+//                if (!traceFileStream.is_open())
+//                {
+//                    std::cerr << "Error opening trace file";
+//                    traceFileStream.close();
+//                    exit(EXIT_FAILURE);
+//                }
+//                break;
+//            default:
+//                fprintf(stderr,
+//                        "Usage: ./csim [-v] -s <s> -E <E> -b <b> -t <tracefile>");
+//                exit(EXIT_FAILURE);
+//        }
+//    }
+//    return 0;
+//}
 
-void Simulator::runTraceFile(std::ifstream traceFileStream) {
+void Simulator::runTraceFile() {
     int processorID;
     std::string operation;
     std::string addressString;
     long address;
 
     int timeStamp = 0;
-    while (traceFileStream >> processorID >> operation >> addressString);
+    while (traceFileStream >> processorID >> operation >> addressString)
     {
         // Convert hexadecimal string into long
         std::istringstream iss(addressString);
         iss >> std::hex >> address;
 
         // Execute trace line
-        cacheControllers[processorID].runCacheOp(address, operation, 0);
+        nodeSmartPointerControllers[processorID].get()->runCacheOp(address, operation, 0);
 
         timeStamp++;
     }
+}
+
+void Simulator::printSummary() {
+    statistics.printSummary();
+}
+
+Simulator::Simulator(int s, int E, int b, int n, int verbose, std::string traceFilePath, const TOPO topo) :
+        s(s), E(E), b(b), n(n), verbose(verbose),
+        traceFileStream(traceFilePath) ,interconnection(topo, nodeSmartPointerControllers), statistics() {
+    if (!traceFileStream.is_open())
+    {
+        std::cerr << "Error opening trace file";
+        traceFileStream.close();
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::shared_ptr<NodeController> nodeControllerPointer
+                = std::make_shared<SnoopingCacheController>(s, E, b, i, interconnection, statistics);
+        nodeSmartPointerControllers.push_back(nodeControllerPointer);
+    }
+
+    interconnection = Interconnection(topo, nodeSmartPointerControllers);
+    statistics = Statistics();
 }
--- src/Simulator.h
+++ src/Simulator.h
@@ -1,30 +1,38 @@
 #ifndef PROJECT_CSIM_SIMULATOR_H
 #define PROJECT_CSIM_SIMULATOR_H
 
-#include <String>
+#include <string>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <getopt.h>
+#include "SnoopingCacheController.h"
 #include "Cache.h"
 #include "Statistics.h"
 #include "Interconnection.h"
 
 class Simulator {
 private:
-    std::vector<NodeController> cacheControllers;
-    Statistics & statistics;
+    std::vector<std::shared_ptr<NodeController>> nodeSmartPointerControllers;
+    Statistics statistics;
     Interconnection interconnection;
-    int getOptions(int argc, char* argv[]);
+//    int getOptions(int argc, char* argv[]);
 
     int s;  // Number of set index bits (2^s is the cache size, or the number of sets in cache)
     int E;  // Associativity
     int b;  // Number of block bits
+    int n;  // Number of processors
 
     int verbose;
 
     std::ifstream traceFileStream;
 
 public:
+    Simulator(int s, int E, int b, int n, int verbose, std::string traceFilePath, const TOPO topo);
 
-    void runTraceFile(std::ifstream);
+    void runTraceFile();
+
+    void printSummary();
 };
 
 
