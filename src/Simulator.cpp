426ece1b94d4c020ad926a46779c517ad3aca3eb
SherwinKing <jinsheng1997@outlook.com>
2021-05-11 17:23:09 +0800

    Delete unnecessary code

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -1,39 +1,6 @@
 #include "Simulator.h"
 
 
-//int Simulator::getOptions(int argc, char **argv) {
-//    int option;
-//    while( (option = getopt(argc, argv, "hvs:E:b:t:")) != -1 ){
-//        switch(option){
-//            case 'v':
-//                verbose = 1;
-//                break;
-//            case 's':
-//                s = atoi(optarg);
-//                break;
-//            case 'E':
-//                E = atoi(optarg);
-//                break;
-//            case 'b':
-//                b = atoi(optarg);
-//                break;
-//            case 't':
-//                traceFileStream.open("example.txt");
-//                if (!traceFileStream.is_open())
-//                {
-//                    std::cerr << "Error opening trace file";
-//                    traceFileStream.close();
-//                    exit(EXIT_FAILURE);
-//                }
-//                break;
-//            default:
-//                fprintf(stderr,
-//                        "Usage: ./csim [-v] -s <s> -E <E> -b <b> -t <tracefile>");
-//                exit(EXIT_FAILURE);
-//        }
-//    }
-//    return 0;
-//}
 
 void Simulator::runTraceFile() {
     int processorID;
