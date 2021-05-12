7c31774932741f7a7c3d3671169331acaa014a81
Charles Qiu <qiuje98@outlook.com>
2021-05-12 17:34:45 -0500

    Modified for Token cache controller testing

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -42,13 +42,23 @@ Simulator::Simulator(int s, int E, int b, int n, int verbose, std::string traceF
                 = std::make_shared<SnoopingCacheController>(s, E, b, i, interconnection, statistics);
         nodeSmartPointerControllers.push_back(nodeControllerPointer);
         */
+
         // n cache controllers
+        // Directory
+        /*
         std::shared_ptr<NodeController> nodeControllerPointer = 
             std::make_shared<DirectoryCacheController>(s, E, b, i, interconnection, statistics);
+        */
+        // Token
+        std::shared_ptr<NodeController> nodeControllerPointer = 
+            std::make_shared<TokenCacheController>(s, E, b, i, interconnection, statistics);
         nodeSmartPointerControllers.push_back(nodeControllerPointer);
     }
     // one cache directory
+    /*
     nodeSmartPointerControllers.push_back(std::make_shared<Directory>(n, b, s, DIRECTORY));
+    */
+    nodeSmartPointerControllers.push_back(std::make_shared<Directory>(n, b, s, TOKEN));
 
     interconnection = Interconnection(topo, nodeSmartPointerControllers);
     statistics = Statistics();
