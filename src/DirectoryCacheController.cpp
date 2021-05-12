8d55fd92ca15bf8e2840fb80bc388f451d2fb620
Charles Qiu <qiuje98@outlook.com>
2021-05-12 11:50:03 -0500

    Add directory cache controller

---
+++ src/DirectoryCacheController.cpp
@@ -0,0 +1,218 @@
+#include "DirectoryCacheController.h"
+#include "CacheSet.h"
+
+void DirectoryCacheController::updateCacheLine(CacheLine &cacheLineToUpdate, long newTag, std::string operation,
+                                              int timeStamp) {
+    cacheLineToUpdate.tag = newTag;
+    //std::cerr << "updateCacheLine << cacheLineToUpdate.tag: " << cacheLineToUpdate.tag << std::endl;
+    //std::cerr << "in updateCacheLine, address: " << &cacheLineToUpdate;
+    cacheLineToUpdate.isEmpty = 0;
+    cacheLineToUpdate.lastUseTimestamp = timeStamp;
+    cacheLineToUpdate.dirtyBit = (operation == "S") ? 1 : 0;
+}
+
+
+void DirectoryCacheController::runCacheOp(long address, std::string operation, int timeStamp) {
+
+    long setID = (address & setIDMask) >> b;
+    long tag = (address & tagMask) >> (s+b);
+
+    CacheLine * cacheLinePtr;
+
+    // Cache hit
+    if (cache.ifCacheLinePresent(setID, tag)) {
+        statistics.cacheHit(processorID, address);
+
+        cacheLinePtr = & cache.getCacheLine(setID, tag);
+    }
+    // Cache miss
+    else {
+        statistics.cacheMiss(processorID, address);
+
+        // Find an empty cache block, or evict one when necessary. Then update the cache line.
+        cacheLinePtr = & cache.findCacheBlockToReplace(setID);
+        // Check if the cache line is to be evicted
+        if (!cacheLinePtr->isEmpty) {
+            statistics.cacheEvict(processorID, cacheLinePtr->setID, cacheLinePtr->tag);
+            // TODO send message to directory
+            interconnection.sendEviction(
+            //cacheLinePtr->isEmpty = 1;
+        }
+    }
+
+
+    updateCacheLine(*cacheLinePtr, tag, operation, timeStamp);
+    transitCacheLineStateOnOperation(*cacheLinePtr, address, operation);
+}
+
+/**
+ * Cache hears requests on bus.
+ * @param request
+ * @return
+ */
+Response DirectoryCacheController::requestHandler(Request request, int sourceID) {
+    Response response = Response(ACK_SHARED);
+    long address = request.getRequestAddress();
+    long setID = (address & setIDMask) >> b;
+    long tag = (address & tagMask) >> (s+b);
+    // cache must have the line in order to receive the request
+    assert(cache.ifCacheLinePresent(setID, tag));
+    CacheLine & cacheLine = cache.getCacheLine(setID, tag);
+    // cache line must not be in I state
+    assert(cacheLine.coherenceState.mesiState != I);
+    transitCacheLineStateOnRequest(cacheLine, request.getRequestAddress(), request);
+    return response;
+
+    /*
+    Response response = Response(ACK_NOT_SHARED);
+    long address = request.getRequestAddress();
+    long setID = (address & setIDMask) >> b;
+    long tag = (address & tagMask) >> (s+b);
+    if (cache.ifCacheLinePresent(setID, tag)) {
+        CacheLine & cacheLine = cache.getCacheLine(setID, tag);
+        transitCacheLineStateOnRequest(cacheLine, request.getRequestAddress(), request);
+        response.setResponseType(ACK_SHARED);
+    }
+    return response;
+    */
+}
+
+void DirectoryCacheController::transitCacheLineStateOnRequest(CacheLine &cacheLine, long cacheAddress, Request request) {
+    switch(request.getRequestType()) {
+        case BusRd:
+            if (cacheLine.coherenceState.mesiState == M) {
+                statistics.cacheFlush(processorID, cacheAddress);
+            }
+            if (cacheLine.coherenceState.mesiState != I) {
+                //printf("Cache[%d] in S   |   ", processorID);
+                cacheLine.coherenceState.mesiState = S;
+            }
+            break;
+        case BusRdX:
+            if (cacheLine.coherenceState.mesiState == M) {
+                statistics.cacheFlush(processorID, cacheAddress);
+            }
+            if (cacheLine.coherenceState.mesiState != I) {
+                statistics.cacheInvalidate(processorID, cacheAddress);
+            }
+            cacheLine.coherenceState.mesiState = I;
+            //cacheLine.isEmpty = 1;
+            break;
+        default:
+            throw "Invalid state: the request type is not supported!";
+    }
+}
+
+
+void DirectoryCacheController::transitCacheLineStateOnOperation(CacheLine &cacheLine, long cacheAddress, std::string operation) {
+
+    //printf("\n*********************\n");
+    if (operation == "L") {
+        //printf("Cache[%d] has L operation\n", processorID);
+        switch (cacheLine.coherenceState.mesiState) {
+            case MESIState::I: {
+                // send request to directory first
+                int directoryLatency = 0;
+                int cacheLatency = 0;
+                RequestType directoryRequestType = DIRECTORY_REQUEST_R;
+                Request directoryRequest(directoryRequestType, coherenceType, cacheAddress);
+                Response directoryResponse = interconnection.sendRequest(processorID, -1, directoryRequest, directoryLatency);
+                // if empty cache owner id, go to state E, S otherwise
+                if(directoryResponse.getCacheOwnerIDs.size() == 0){
+                    cacheLine.coherenceState.mesiState = MESIState::E;
+                }
+                else{
+                    cacheLine.coherenceState.mesiState = MESIState::S;
+                    RequestType cacheRequestType = BusRd;
+                    Request cacheRequest(cacheRequestType, coherenceType, cacheAddress);
+                    for(auto ownerID : directoryResponse.getCacheOwnerIDs()){
+                        Response cacheResponse = interconnection.sendRequest(processorID, ownerID, cacheRequest, cacheLatency);
+                        // TODO need to calculate the latency somehow
+                        
+                    }
+                }
+
+                break;
+            }
+            case MESIState::S:
+            case MESIState::E:
+            case MESIState::M:
+                // No action
+                break;
+            default:
+                throw "Illegal state: the mesi state is illegal!";
+        }
+    } else {    // operation == "S"
+        //printf("Cache[%d] has S operation\n", processorID);
+        switch (cacheLine.coherenceState.mesiState) {
+            case MESIState::I:
+            case MESIState::S: {
+                // send request to directory first
+                int directoryLatency = 0;
+                int cacheLatency = 0;
+                RequestType directoryRequestType = DIRECTORY_REQUEST_W;
+                Request directoryRequest(directoryRequestType, coherenceType, cacheAddress);
+                Response directoryResponse = interconnection.sendRequest(processorID, -1, directoryRequest, directoryLatency);
+                // invalidata all other caches having this line
+                RequestType cacheRequestType = BusRdX;
+                Request cacheRequest(cacheRequestType, coherenceType, cacheAddress);
+                for(auto ownerID : directoryResponse.getCacheOwnerIDs()){
+                    Response cacheResponse = interconnection.sendRequest(processorID, ownerID, cacheRequest, cacheLatency);
+                    // TODO
+
+                }
+
+                break;
+                /*
+                // Broadcast request
+                RequestType requestType = BusRdX;
+                Request request(requestType, coherenceType, cacheAddress);
+                std::vector<Response> responseVector;
+                interconnection.broadcastRequest(processorID, request, responseVector);
+                break;
+                */
+            }
+            case MESIState::E: {
+                // send request to directory only for updating dirty bit
+                int directoryLatency = 0;
+                int cacheLatency = 0;
+                RequestType directoryRequestType = DIRECTORY_REQUEST_W;
+                Request directoryRequest(directoryRequestType, coherenceType, cacheAddress);
+                Response directoryResponse = interconnection.sendRequest(processorID, -1, directoryRequest, directoryLatency);
+                // NO other cache will have this line
+                assert(directoryResponse.getCacheOwnerIDs().size() == 0);
+            }
+            case MESIState::M:
+                // No action
+                break;
+            default:
+                throw "Illegal state: the mesi state is illegal!";
+        }
+        //printf("Cache[%d] in M   |   ", processorID);
+        cacheLine.coherenceState.mesiState = MESIState::M;
+    }
+}
+
+DirectoryCacheController::DirectoryCacheController(int s, int E, int b,
+                                                 int processorID, Interconnection &interconnection,
+                                                 Statistics &statistics) : s(s), E(E), b(b),
+                                                                           coherenceType(SNOOPING),
+                                                                           processorID(processorID),
+                                                                           interconnection(interconnection),
+                                                                           cache(processorID, s, E, b),
+                                                                           statistics(statistics) {
+    setIDMask = ((1<<s) - 1) << b;
+    tagMask = ~((1<<(s+b))-1);
+//    this->s = s;
+//    this->E = E;
+//    this->b = b;
+//    this->processorID = processorID;
+//    this->interconnection = interconnection;
+//    this->statistics = statistics;
+//    cache = Cache(processorID, s, E, b);
+}
+
+void DirectoryCacheController::evictionHandler(int sourceID, long setID, long tag, int token) {
+    return;
+}
+
---
+++ src/DirectoryCacheController.h
@@ -0,0 +1,47 @@
+
+#ifndef PROJECT_CSIM_DIRECTORYCACHECONTROLLER_H
+#define PROJECT_CSIM_DIRECTORYCACHECONTROLLER_H
+
+#include <string>
+#include "NodeController.h"
+#include "Cache.h"
+#include "Interconnection.h"
+#include "Statistics.h"
+#include "Response.h"
+#include "Request.h"
+
+class DirectoryCacheController: public NodeController {
+private:
+    int id;
+    int s;  // Number of set index bits (2^s is the cache size, or the number of sets in cache)
+    int E;  // Associativity
+    int b;  // Number of block bits
+
+    long setIDMask;
+    long tagMask;
+
+    const CoherenceType coherenceType = DIRECTORY;
+    int processorID;
+    Cache cache;
+    Interconnection & interconnection;
+    Statistics & statistics;
+
+    void transitCacheLineStateOnRequest(CacheLine &cacheLine, long cacheAddress, Request request) override ;
+
+    void transitCacheLineStateOnOperation(CacheLine &cacheLine, long cacheAddress, std::string operation)  ;
+
+    void updateCacheLine(CacheLine &cacheLineToUpdate, long newTag, std::string operation, int timeStamp);
+
+public:
+    DirectoryCacheController(int s, int E, int b, int processorID,
+                            Interconnection &interconnection, Statistics &statistics);
+
+    void runCacheOp(long address, std::string operation, int timeStamp) override;
+
+    Response requestHandler(Request request, int sourceID) override;
+
+    void evictionHandler(int sourceID, long setID, long tag, int token) override;
+};
+
+
+#endif //PROJECT_CSIM_DIRECTORYCACHECONTROLLER_H
